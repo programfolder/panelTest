0b367a78b7131ae8cc5a59fe926e5322299a0302
LIN JOHN <1483575447@QQ.com>
2021-12-24 11:38:15 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace panelTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(212, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(81, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 323);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "panel测试";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace panelTest
+{
+    public partial class Form1 : Form
+    {
+        public List<Button> list = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+                button.Click += Button_Click;
+                button.Location = new Point(20,50 + i*20);
+                button.Size = new Size(100, 20);
+                button.Text = "click" + @"  " + i.ToString();
+                button.Name = i.ToString();
+                list.Add(button);
+
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int clickednumber = int.Parse(btn.Name);
+                 if(list.All(item => item.Visible == true))
+                 {
+                    for (int i = 0; i < 5; i++)
+                    {
+                             if (i != clickednumber)
+                             {
+                                    list[i].Visible = false;
+                             }
+                              else if (i == clickednumber)
+                              list[clickednumber].Location = new Point(20, 50);
+                    }
+                 }
+                else
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        list[i].Location = new Point(20, 50 + i * 20);
+                        list[i].Visible = true;
+                    }
+
+                }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("it is me");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5};            
+            list.Remove( list.Count );
+            list.ForEach(item => MessageBox.Show(item.ToString()));
+        }
+    }
+}
